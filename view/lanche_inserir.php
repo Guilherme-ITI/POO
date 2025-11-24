8008f81c04091367e2daf426528e690ed6cfd942
Guilherme Oliveira <guilhermsantosoliveira982@gmail.com>
2025-11-24 10:41:54 -0300

    formulario de htdocs

---
+++ view/lanche_inserir.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="view/style.css">
+</head>
+<body>
+    <main>
+        <form action= "" method="post">
+        <h1>Lanche :: Inserir</h1>    
+        <label> Nome do lanche </label>
+        <input type="text" name="txtNome"/>
+        <label for="">Ingredientes</label>
+        <input type="text" name="txtIngredientes"/>
+        <label for="txtPreco">Preco</label>
+        <input type="text" name="txtPreco"/>
+
+        <button type="submit">Salvar</button>
+
+
+        </form>
+    </main>
+</body>
+</html>
